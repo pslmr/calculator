36e4cb24c35a6c492783e18576f6d4285535eeca
Psalmer Abalon <abalonpsalmer@gmail.com>
2022-08-12 18:11:18 +0800

    Add click eventlisteners to buttons

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ function divide(num1, num2) {
 	return num1 / num2;
 }
 
+function remainder(num1, num2) {
+	return num1 % num2;
+}
+
 function operate(num1, num2, ope) {
 	switch (ope) {
 		case "+":
@@ -32,7 +36,51 @@ function operate(num1, num2, ope) {
 			//divide(num1, num2);
 			console.log(divide(num1, num2));
 			break;
+		case "%":
+			// remainder(num1, num2);
+			console.log(remainder(num1, num2));
+			break;
 		default:
 			console.log("Error");
 	}
 }
+
+const numBtn = document.querySelectorAll(".button.number");
+const opeBtn = document.querySelectorAll(".button.operator");
+const clearAll = document.querySelector(".button.all-clear");
+const backspace = document.querySelector(".button.clear");
+const equals = document.querySelector(".button.equals");
+const display = document.querySelector(".display");
+const displayCurrent = document.createElement("p");
+display.appendChild(displayCurrent);
+let num = "";
+
+numBtn.forEach((button) =>
+	button.addEventListener("click", (e) => {
+		num += e.target.innerText;
+
+		displayCurrent.innerText = num;
+	})
+);
+
+opeBtn.forEach((button) =>
+	button.addEventListener("click", (e) => {
+		num += e.target.innerText;
+
+		displayCurrent.innerText = num;
+	})
+);
+
+clearAll.addEventListener("click", () => {
+	num = "";
+	displayCurrent.innerText = "";
+});
+
+backspace.addEventListener("click", () => {
+	splitNum = Array.from(num);
+	splitNum.pop();
+	num = splitNum.join("");
+	displayCurrent.innerText = num;
+});
+
+equals.addEventListener("click", () => {});
